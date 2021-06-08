6b96d5000d6b38483d236e0f5b201b7e1b3305f8
Lumy <lumy@lumy.me>
2021-06-08 16:56:02 +0200

    removing useless file from subject

--- part3/dwarfs/dwarf_army.py
+++
@@ -1,7 +0,0 @@
-
-class Army():
-  """
-  List representing the army on the battlefield.
-  They are in order/line. They fight first one against first one of other army.
-  Knight can move: methods Army.move can be called with index of a knight and how many case to move.
-  """
--- part3/dwarfs/dwarf_camp.py
+++
@@ -1,7 +0,0 @@
-"""
-  inheritance of base.Training camp. Limit/Extend for elves/dwarf some class
-
-"""
-
-class TrainingCamp:
-  pass
